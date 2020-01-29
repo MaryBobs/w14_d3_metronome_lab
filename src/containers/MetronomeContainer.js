b5515cbe0c26a3a6973deeaa5ac52b2a7e6aabb9
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-29 10:27:32 +0000

    adds Metrnnome Contationer

---
+++ src/containers/MetronomeContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MetronomeContainer extends Component {
+    render() {
+        return (
+            <p>Hello Metronome!!</p>
+        )
+    }
+}
+
+export default MetronomeContainer;
